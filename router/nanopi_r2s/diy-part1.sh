dce4322d9087e436057abe5a90d7343d58f6f1fb
Joseph Smith <josephasmith1@gmail.com>
2020-11-16 08:10:50 -0800

    Update diy-part1.sh

--- router/nanopi_r2s/diy-part1.sh
+++ router/nanopi_r2s/diy-part1.sh
@@ -8,14 +8,8 @@
 #========================================================================================================================
 
 # Uncomment a feed source
-# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '/openwrt_luci/ { s/snapshots\/packages/releases\/packages-19.07/g; }' /etc/opkg/distfeeds.conf
-sed -i '/openwrt_packages/ { s/snapshots\/packages/releases\/packages-19.07/g; }' /etc/opkg/distfeeds.conf
-sed -i '/openwrt_routing/ { s/snapshots\/packages/releases\/packages-19.07/g; }' /etc/opkg/distfeeds.conf
-sed -i '/openwrt_telephony/ { s/snapshots\/packages/releases\/packages-19.07/g; }' /etc/opkg/distfeeds.conf
-sed -i '/natelol/d' /etc/opkg/distfeeds.conf
-sed -i 's,downloads.openwrt.org,mirrors.cloud.tencent.com/lede,g' /etc/opkg/distfeeds.conf
